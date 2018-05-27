0dac9fee7e20c6dac8d268f1896a91d487a795d7
Bruno Dutra <brunocodutra@gmail.com>
2018-05-27 22:59:15 +0200

    Squashed 'cmake/' changes from bec9120..a754b40
    a754b40 add support for code coverage
    
    git-subtree-dir: cmake
    git-subtree-split: a754b40a1e10a13a9b1ee60445659ff144abab39

--- include/alloy/config.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef ALLOY_CONFIG_HPP
-#define ALLOY_CONFIG_HPP
-
-#include "config/version.hpp"
-
-#endif
--- include/alloy/sink.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef ALLOY_SINK_HPP
-#define ALLOY_SINK_HPP
-
-#include "sink/model.hpp"
-
-#endif
